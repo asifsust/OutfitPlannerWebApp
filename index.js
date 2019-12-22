b3d7e6067575f9e02255f8c97369f79209856311
Dennis Duka <GreyTeddy@users.noreply.github.com>
2019-12-22 15:09:27 +0000

    Initial ExpressJS and example HTML

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require ('express');
+
+const app = express();
+const port = 8000;
+
+require('./routes/main')(app);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
