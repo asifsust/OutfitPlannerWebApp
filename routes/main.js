ca706e66ecf92bb3e430176c1db8513b5b5b25ee
Dennis Duka <GreyTeddy@users.noreply.github.com>
2020-01-15 14:15:15 +0000

    added routes to the other pages added

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,14 @@ module.exports = function(app)
     app.get('/about',function(req,res){
         res.render('about.html');
     });
+    app.get('/forgot',function(req,res){
+        res.render('forgot.html');
+    });
+    app.get('/login',function(req,res){
+        res.render('login.html');
+    });
+    app.get('/signup',function(req,res){
+        res.render('signup.html');
+    });
+    
 }
